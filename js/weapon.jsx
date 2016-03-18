2b43233f2e799ecf24f6880143e32fdbd2bcd7bf
philmist <philmist.j@gmail.com>
2016-03-18 18:08:59 +0900

    とりあえず武器の体裁だけととのえた。

--- js/weapon.jsx
+++ js/weapon.jsx
@@ -11,7 +11,83 @@ var Weapon = React.createClass({
     return (
       <section>
         <header className="subtype">武器</header>
+        <form name="weapon">
+          <table className="grbr" id="weapon_tabel" ref="weapon_table">
+            <thead>
+              <tr>
+                <th>鍵</th>
+                <th>選</th>
+                <th className="width150">名前</th>
+                <th className="width50">攻撃力</th>
+                <th>種別</th>
+                <th>スキル1</th>
+                <th>スキル2</th>
+                <th>LV</th>
+                <th>並替・挿入・削除</th>
+              </tr>
+            </thead>
+            <tbody>
+              <WeaponRows
+                parameter={this.props.parameter}
+                onChangeParameter={this.props.onChangeParameter}
+              />
+            </tbody>
+          </table>
+        </form>
       </section>
+    );
+  }
+});
+
+
+var WeaponRows = React.createClass({
+  render: function() {
+    return (
+      <WeaponRow />
+    );
+  }
+});
+
+
+var WeaponRow = React.createClass({
+  render: function() {
+    return (
+      <tr>
+        <td>
+          <input type="checkbox" className="weapon_lock" />
+        </td>
+        <td>
+          <input type="checkbox" className="weapon_select" />
+        </td>
+        <td>
+          <input type="text" className="weapon_name width150" />
+        </td>
+        <td>
+          <input type="text" className="weapon_atk width50" />
+        </td>
+        <td>
+          <select className="weapon_kind">
+          </select>
+        </td>
+        <td>
+          <select className="weapon_skill_type1">
+          </select>
+        </td>
+        <td>
+          <select className="weapon_skill_type2">
+          </select>
+        </td>
+        <td>
+          <select className="weapon_skill_lv">
+          </select>
+        </td>
+        <td>
+          <input type="button" id="up" value="▲" />
+          <input type="button" id="down" value="▼" />
+          <input type="button" id="ins" value="+" />
+          <input type="button" id="del" value="-" />
+        </td>
+      </tr>
     );
   }
 });
