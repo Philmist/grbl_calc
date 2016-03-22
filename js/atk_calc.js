bb17921151b29c2f815c43af41701e64c7b91512
philmist <philmist.j@gmail.com>
2016-03-22 12:00:25 +0900

    計算式を最新に合わせたつもり。

--- js/atk_calc.js
+++ js/atk_calc.js
@@ -19,7 +19,8 @@
           atk: 武器攻撃力(Number),
           type: 武器の種類を表わした文字列(String),
           skill_level: 武器のスキルレベル(Number),
-          skill_type: [武器のスキル1つ目の種別の文字列(String), 同2つ目 ]
+          skill_type: [武器のスキル1つ目の種別の文字列(String), 同2つ目 ],
+          cosmos: コスモス武器か否か(boolean)
         }, ...
       ],
     summon: [ // 召喚1つを配列の(ry
@@ -83,12 +84,12 @@ export default function calculate_atkval (param_obj, job_data) {
   // 武器攻撃力の計算
   showed_atk += function () {  // 表示攻撃力に処理で得られた総合武器攻撃力を加算する
     var total_atk = 0;
-    var zenith_bonus = [0, 1, 3, 5];  // 各zenithの星に対応する追加ボーナス%
+    const zenith_bonus = [0, 1, 3, 5, 0, 0, 10];  // 各zenithの星に対応する追加ボーナス%
     param_obj.weapon.forEach(function(weapon) {
-      var atk = weapon.atk;  // 基礎攻撃力
-      var specialty_basic = 100;  // 得意武器倍率
-      var specialty_bonus = 0;  // Zenith追加%
-      var job = job_data[param_obj.job];  // 該当ジョブのデータを取得
+      let atk = weapon.atk;  // 基礎攻撃力
+      let specialty_basic = 100;  // 得意武器倍率%
+      let specialty_bonus = 0;  // Zenith追加%
+      let job = job_data[param_obj.job];  // 該当ジョブのデータを取得
       if (job) {  // もし該当ジョブが存在するのなら
         // 得意武器の一覧を見て...
         for (var i = 0; i < job.specialty.length; i++) {
@@ -100,8 +101,9 @@ export default function calculate_atkval (param_obj, job_data) {
           }
         }
       }
+      let specialty_cosmos = weapon.cosmos ? 30 : 0; // コスモス武器の追加%
       // 武器攻撃力に倍率をかける
-      atk = atk * (specialty_basic + specialty_bonus) / 100;
+      atk = atk * (specialty_basic + specialty_bonus + specialty_cosmos) / 100;
       // 全武器攻撃力を更新する
       total_atk += atk;
     });
@@ -180,9 +182,10 @@ export default function calculate_atkval (param_obj, job_data) {
       total_skill[type_str]["backwater"] += hp_coefficient * bw_coefficient / 3;
     };
   }
-  var CHECK_LEVEL = 10;
-  //// 渡された数値がCHECK_LEVELより下かどうかをチェックする関数
-  function less_than_chklv(n) { return (n < CHECK_LEVEL); }
+  //// 渡された数値がlevelより下かどうかをチェックする関数を返す関数
+  function less_than_chklv(level) {
+    return function less_than_chklv(n) { return (n < level); }
+  }
   //// addval + (lv - subval) * mulvalを計算する関数を返す関数
   function pcalc_gen(addval, subval, mulval) {
     return function(lv) {
@@ -190,73 +193,80 @@ export default function calculate_atkval (param_obj, job_data) {
     };
   }
   /// スキルと関数を対応させる
+  const CHECK_LEVEL = 10;
   var skill_calc_dict = {
+    // total_skill.kj1.percent = 0 + (lv - 0) * 1  [lv<10]
+    // total_skill.kj1.percent = 10 + (lv - 10) * 0.4 [lv>=10]
     "kj1": pfunc_gen(
-      "koujin", less_than_chklv,
+      "koujin", less_than_chklv(CHECK_LEVEL),
       pcalc_gen(0, 0, 1), pcalc_gen(10, CHECK_LEVEL, 0.4)
     ),
     "kj2": pfunc_gen(
-      "koujin", less_than_chklv,
+      "koujin", less_than_chklv(CHECK_LEVEL),
       pcalc_gen(2, 0, 1), pcalc_gen(12, CHECK_LEVEL, 0.5)
     ),
     "kj3": pfunc_gen(
-      "koujin", less_than_chklv,
+      "koujin", less_than_chklv(CHECK_LEVEL),
       pcalc_gen(5, 0, 1), pcalc_gen(15, CHECK_LEVEL, 0.6)
     ),
     "kj4": pfunc_gen(
-      "koujin", less_than_chklv,
+      "koujin", less_than_chklv(CHECK_LEVEL),
       pcalc_gen(6, 0, 1), pcalc_gen(16, CHECK_LEVEL, 0.8)
     ),
     "mkj1": pfunc_gen(
-      "magna", less_than_chklv,
+      "magna", less_than_chklv(CHECK_LEVEL),
       pcalc_gen(2, 0, 1), pcalc_gen(12, CHECK_LEVEL, 0.5)
     ),
     "mkj2": pfunc_gen(
-      "magna", less_than_chklv,
+      "magna", less_than_chklv(CHECK_LEVEL),
       pcalc_gen(5, 0, 1), pcalc_gen(15, CHECK_LEVEL, 0.6)
     ),
+    "mkm1": pfunc_gen(
+      "magna", less_than_chklv(CHECK_LEVEL),
+      (lv) => (lv), (lv) => (10 + (lv - CHECK_LEVEL) * 0.4)
+    ),
     "unk1": pfunc_gen(
-      "unknown", less_than_chklv,
-      pcalc_gen(2, 0, 1), pcalc_gen(12, 0, 0)  // (15,0,0) -> 15 + (lv - 0) * 0
+      "unknown", less_than_chklv(CHECK_LEVEL),
+      pcalc_gen(2, 0, 1), pcalc_gen(12, 0, 0)  // (12,0,0) -> 12 + (lv - 0) * 0
     ),
     "unk2": pfunc_gen(
-      "unknown", less_than_chklv,
+      "unknown", less_than_chklv(CHECK_LEVEL),
       pcalc_gen(5, 0, 1), pcalc_gen(15, 0, 0)
     ),
     "str": pfunc_gen(
-      "collabo", less_than_chklv,
+      "collabo", less_than_chklv(CHECK_LEVEL),
       pcalc_gen(5, 0, 1), pcalc_gen(15, 0, 0)
     ),
     "bha": pfunc_gen(
-      "baha", less_than_chklv,
+      "baha", less_than_chklv(CHECK_LEVEL),
       pcalc_gen(19, 0, 1), pcalc_gen(30, 0, 0)
     ),
     "bhah": pfunc_gen(
-      "baha", less_than_chklv,
+      "baha", less_than_chklv(CHECK_LEVEL),
       pcalc_gen(9.5, 0, 0.5), pcalc_gen(15, 0, 0)
     ),
     "bw1": bwfunc_gen(
-      "normal", less_than_chklv,
+      "normal", less_than_chklv(CHECK_LEVEL),
       (l) => (-0.3 + l * 1.8),
       (l) => (18 + (l - CHECK_LEVEL) / 5 * 3)
     ),
     "bw2": bwfunc_gen(
-      "normal", less_than_chklv,
+      "normal", less_than_chklv(CHECK_LEVEL),
       (l) => (-0.4 + l * 2.4),
       (l) => (24 + (l - CHECK_LEVEL) / 5 * 3)
     ),
     "bw3": bwfunc_gen(
-      "normal", less_than_chklv,
+      "normal", less_than_chklv(CHECK_LEVEL),
       (l) => (-0.5 + l * 3.0),
       (l) => (30 + (l - CHECK_LEVEL) / 5 * 3)
     ),
     "mbw1": bwfunc_gen(
-      "magna", less_than_chklv,
+      "magna", less_than_chklv(CHECK_LEVEL),
       (l) => (-0.3 + l * 1.8),
       (l) => (18 + (l - CHECK_LEVEL) / 5 * 3)
     ),
     "mbw2": bwfunc_gen(
-      "magna", less_than_chklv,
+      "magna", less_than_chklv(CHECK_LEVEL),
       (l) => (-0.5 + l * 3.0),
       (l) => (30 + (l - CHECK_LEVEL) / 5 * 3)
     )
@@ -271,7 +281,6 @@ export default function calculate_atkval (param_obj, job_data) {
   });
 
   // 総合計算
-  showed_atk = Math.round(showed_atk);
   var total_atk = showed_atk;
   total_atk *= (100 + (total_skill.baha.percent + total_skill.koujin.percent * divine_percent.zeus / 100)) / 100;
   total_atk *= (100 + total_skill.normal.backwater * divine_percent.zeus / 100) / 100;
@@ -280,7 +289,9 @@ export default function calculate_atkval (param_obj, job_data) {
   total_atk *= (100 + (total_skill.collabo.percent + total_skill.unknown.percent * divine_percent.unknown / 100)) / 100;
   total_atk *= (divine_percent.attribute + attribute_bonus) / 100;
   total_atk *= (100 + param_obj.ship_bonus) / 100;
+
   total_atk = Math.round(total_atk);
+  showed_atk = Math.round(showed_atk);
 
   // 結果の返却
   return {
