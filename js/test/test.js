4eb5a4fff584541a859f20697edb821cd4cf109a
philmist <philmist.j@gmail.com>
2016-05-14 15:52:40 +0900

    テストケースを追加。
    * 一部パッケージはとりあえずバージョンを指定しないことにした。
    * テストケースを2つほど追加した。

--- js/test/test.js
+++ js/test/test.js
@@ -4,6 +4,55 @@ import { expect } from 'chai';
 import get_job_data from "../get_job_data.js";
 import calculate_atkval from "../atk_calc.js";
 
+
+// 以下の値はテストケース内で変更して使うことを想定している
+const param_for_test = {
+  rank: 1,  // ランク
+  ship_bonus: 0,  // 騎空艇補正
+  hp_percent: 100,  // 現HPの割合(%)
+  job: "fighter",  // "data/job_data.json"で定義されている職業(クラス)
+  affinity: "none",  // 相性(none/good/bad)
+  zenith: {  // Zenith Perk
+    atk: 0,  // 攻撃力の星(0-3)
+    weapon: [0, 0],  // 武器1, 武器2の星(0-3)
+    attribute: 0  // 属性攻撃力の星(0-3)
+  },
+  weapon: [  // 武器
+    /*
+       {
+       atk: 100,
+       type: "sword",
+       skill_level: 1,
+       skill_type: ["none", "none"]
+       },
+       {
+       atk: 1000,
+       type: "stuff",
+       skill_level: 20,
+       skill_type: ["kj4", "bw3"]
+       }
+       */
+  ],
+  summon: [  // 召喚獣
+    {
+      atk: 0,
+      skill: [
+        /*
+           {
+           type: "attribute",
+           percent: 50
+           }
+           */
+      ]
+    }
+  ],
+  atk_bonus: {  // 攻撃力ボーナス
+    percent: 0,  // %
+    value: 0  // 値
+  }
+};
+
+
 // mocha+chaiのテストコードサンプル
 describe('Array', function() {
   describe('#indexOf()', function () {
@@ -16,76 +65,74 @@ describe('Array', function() {
 
 
 // calc.jsのテスト
-describe('calc.js', function() {
-  // このテストで使用する変数
-  var param_for_test = {};
-  var job_data;
-  // テスト集合実行前の処理
-  before("職業データを読みこむ", function(){
-    return get_job_data("data/job_data.json").then(function(data) {
+describe('atk_calc.js', function() {
+  // ジョブデータの読みこみ
+  let job_data;
+  before(function(done){
+    get_job_data().then(function(data) {
       job_data = data;
+      done();
     });
   });
-  // テストごとの前処理
-  beforeEach("パラメータのテンプレートを作成", function() {
-    // 以下の値はテストケース内で変更して使うことを想定している
-    // テストごとに値を代入しているのは、一括で代入した場合、参照渡しで変更されるため
-    param_for_test = {
-      rank: 1,  // ランク
-      ship_bonus: 0,  // 騎空艇補正
-      hp_percent: 100,  // 現HPの割合(%)
-      job: "fighter",  // "data/job_data.json"で定義されている職業(クラス)
-      affinity: "none",  // 相性(none/good/bad)
-      zenith: {  // Zenith Perk
-        atk: 0,  // 攻撃力の星(0-3)
-        weapon: [0, 0],  // 武器1, 武器2の星(0-3)
-        attribute: 0  // 属性攻撃力の星(0-3)
-      },
-      weapon: [  // 武器
-        /*
-        {
-          atk: 100,
-          type: "sword",
-          skill_level: 1,
-          skill_type: ["none", "none"]
-        },
-        {
-          atk: 1000,
-          type: "stuff",
-          skill_level: 20,
-          skill_type: ["kj4", "bw3"]
-        }
-        */
-      ],
-      summon: [  // 召喚獣
-        {
-          atk: 0,
-          skill: [
-            /*
-            {
-              type: "attribute",
-              percent: 50
-            }
-            */
-          ]
-        }
-      ],
-      atk_bonus: {  // 攻撃力ボーナス
-        percent: 0,  // %
-        value: 0  // 値
-      }
-    };
+  // テストデータの作成
+  let test_data;
+  beforeEach(function() {
+    test_data = Object.assign(param_for_test);
   });
   // 実際のテスト
-  describe("calculate_atkval()", function() {
+  describe('シンプルな武器攻撃力計算のテスト', function() {
     it('何らかの値を返す', function() {
-      expect(calculate_atkval(param_for_test, job_data)).to.be.ok;
+      expect(calculate_atkval(test_data, job_data)).to.be.ok;
     });
     it('デフォルトでは表示攻撃力は1000', function() {
-      expect(calculate_atkval(param_for_test, job_data).showed_atk).to.equal(1000);
+      expect(calculate_atkval(test_data, job_data).showed_atk).to.equal(1000);
     });
     it('デフォルトでは計算攻撃力は1000', function() {
-      expect(calculate_atkval(param_for_test, job_data).total_atk).to.equal(1000);
+      expect(calculate_atkval(test_data, job_data).total_atk).to.equal(1000);
+    });
+  });
+  describe('攻刃武器単体のテスト(fighter,sword,ATK:100,SLV:10,kj4,kj3)', function() {
+    let result;
+    before('武器を作って計算する', function() {
+      let param;
+      param = Object.assign({}, param_for_test);
+      param.weapon = [{
+        atk: 100,
+        skill_level: 10,
+        skill_type: ['kj4', 'kj3'],
+        type: 'sword'
+      }];
+      param.job = 'fighter';
+      result = calculate_atkval(param, job_data);
+    });
+    it('表示攻撃力は1120', () => {
+      expect(result.showed_atk).to.equal(1120);
+    });
+    it('総合攻撃力は1467', () => {
+      expect(result.total_atk).to.equal(1467);
+    });
+  });
+  describe('背水武器単体のテスト(fighter,sword,ATK:100,SLV:10,bw3,mbw3,hp%:30,affinity:"good")', function() {
+    let result;
+    before('武器を作って計算する', function() {
+      let param;
+      param = Object.assign({}, param_for_test);
+      param.weapon = [{
+        atk: 100,
+        skill_level: 10,
+        skill_type: ['bw3', 'mbw3'],
+        type: 'sword'
+      }];
+      param.job = 'fighter';
+      param.hp_percent = 30;
+      param.affinity = 'good';
+      result = calculate_atkval(param, job_data);
+    });
+    it('表示攻撃力は1120', () => {
+      expect(result.showed_atk).to.equal(1120);
+    });
+    it('総合攻撃力は2292', () => {
+      expect(result.total_atk).to.equal(2292);
     });
   });
 });
