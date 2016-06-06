fe2efbb3fa70ca19c962acf2ec810f39e5d1a5f8
philmist <philmist.j@gmail.com>
2016-06-06 20:47:06 +0900

    Zenith Perk部分を翻訳対応にした。
    * 翻訳用のリソースファイルを追加。
    * Zenith Perkの表示文字列部分を翻訳できるようにした。

---
+++ js/locale/ja.js
@@ -0,0 +1,20 @@
+// vim: sts=2 sw=2 ts=2 expandtab
+
+let text = {
+  zenith: {
+    header: {
+      attack: "攻撃力",
+      weapon1: "得意武器1",
+      weapon2: "得意武器2",
+      attribute: "属性攻撃力"
+    },
+    list: {
+      zero: "無し",
+      one: "★",
+      two: "★★",
+      three: "★★★"
+    }
+  }
+};
+
+export default text;
