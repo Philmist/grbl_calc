4e5feb06806f9f738caba66638eb483e1c5a54a9
philmist <philmist.j@gmail.com>
2016-07-27 15:46:59 +0900

    前処理をするスタブなコードを追加。

--- js/optimize_atk.js
+++ js/optimize_atk.js
@@ -36,25 +36,55 @@ export class GrblFormGAOptimizer {
     this.job_data = job_data;  // job_dataは変更されないことが前提
 
     // 配列を辞書に変換する関数
-    let arytomap = (list) => {
-      let map = { locked: {}, target: {} };
+    let arytoobj = (list) => {
+      let obj = {};
       for (let i = 0; i < list.length; i++) {
         let target = list[i];
-        if (target instanceof Object && target.locked && target.selected) {
-          map.locked[String(i)] = target;
-        } else if (target) {
-          map.target[String(i)] = target;
+        if (target instanceof Object) {
+          if (!("max" in target && target.max >= 0)) {
+            target.max = 1
+          }
+          if (!("min" in target && target.min >= 0)) {
+            target.min = 0
+          }
+          obj[String(i)] = target;
         }
       }
-      return map;
+      return obj;
     }
 
     // 武器配列を辞書に変換する
-    this.weapon_map = arytomap(this.weapon_list);
+    this.weapon_obj = arytoobj(this.weapon_list);
     // 召喚配列を辞書に変換する
-    this.summon_map = arytomap(this.summon_list);
+    this.summon_obj = arytoobj(this.summon_list);
     // フレンド召喚配列を辞書に変換する
-    this.friend_map = arytomap(this.friend_list);
+    this.friend_obj = arytoobj(this.friend_list);
+
+    // 染色体用のリストを作る関数
+    let objtoref = (obj) => {
+      let ref_list = [];
+      for (let i of Object.keys(obj)) {
+        for (let j = 0; j < obj[i].max; j++) {
+          ref_list.push(i);
+        }
+      }
+      return ref_list;
+    };
+
+    // 染色体の参照リストを作る
+    this.weapon_ref = objtoref(this.weapon_obj);
+    this.summon_ref = objtoref(this.summon_obj);
+    this.friend_ref = objtoref(this.friend_obj);
+
+    // 染色体の最大長を設定する
+    // TODO: ハードコーディングなのでもっとマシにする
+    this.weapon_max_chromo_length = this.weapon_ref.length > 10 ? 10 : this.weapon_ref.length;
+    this.summon_max_chromo_length = this.summon_ref.length > 5 ? 5 : this.summon_ref.length;
+    this.friend_max_chromo_length = 1;
+
+    // 初期染色体を作成する
+    // TODO: ここに色々書く
+
     // 状態変数を初期状態に
     this.calc_state.state = CALC_STATE.INITED;
 
