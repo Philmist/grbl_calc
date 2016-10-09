baa3f0e41db6ce540cef2f7d27a1c4eb053d0f48
philmist <philmist.j@gmail.com>
2016-10-09 14:36:20 +0900

    遺伝的アルゴリズムによる最適化計算部分をおおよそ実装。
    * 汚ないコードでもとりあえず動くようにした。
    * 表示への反映は未実装。
    * パラメータはハードコーディング。

--- js/optimize_atk.js
+++ js/optimize_atk.js
@@ -19,7 +19,9 @@ export const CALC_STATE = {
   GA_INITED: "GA_INITED",
   CALC_VALUE: "CALC_VALUE",
   SORT_POPULATION: "SORT_POPULATION",
-  SELECTION_POPULATION: "SELECTION_POPULATION"
+  SELECT_POPULATION: "SELECT_POPULATION",
+  INTERSECT_GENE: "INTERSECT_GENE",
+  MUTATION: "MUTATION"
 };
 
 
@@ -52,7 +54,7 @@ export class GrblFormGAOptimizer {
   // 計算前の初期化関数
   // 計算に必要な各オブジェクトを受けとる
   // 戻り値としてジェネレータを返す
-  // (まだ)再度initされることは想定していない
+  // TODO: (まだ)再度initされることは想定していない
   init(base_param, weapon_list, summon_list, friend_list, job_data) {
     // 必要な変数に代入する
     this.base = Object.assign({}, base_param);
@@ -98,15 +100,17 @@ export class GrblFormGAOptimizer {
     };
 
     // 染色体の参照リストを作る
-    this.weapon_ref = objtoref(this.weapon_obj);
-    this.summon_ref = objtoref(this.summon_obj);
-    this.friend_ref = objtoref(this.friend_obj);
+    this.ref = {
+      weapon: objtoref(this.weapon_obj),
+      summon: objtoref(this.summon_obj),
+      friend: objtoref(this.friend_obj)
+    };
 
     // 染色体の最大長を設定する
     // TODO: ハードコーディングなのでもっとマシにする
-    this.weapon_max_chromo_length = this.weapon_ref.length > 10 ? 10 : this.weapon_ref.length;
-    this.summon_max_chromo_length = this.summon_ref.length > 5 ? 5 : this.summon_ref.length;
-    this.friend_max_chromo_length = this.friend_ref.length > 1 ? 1 : this.friend_ref.length;
+    this.weapon_max_chromo_length = this.ref.weapon.length > 10 ? 10 : this.ref.weapon.length;
+    this.summon_max_chromo_length = this.ref.summon.length > 5 ? 5 : this.ref.summon.length;
+    this.friend_max_chromo_length = this.ref.friend.length > 1 ? 1 : this.ref.friend.length;
 
     // 状態変数を初期状態に
     this.state.status = CALC_STATE.PARAM_INITED;
@@ -146,10 +150,6 @@ export class GrblFormGAOptimizer {
           [tmp_ary[ri], tmp_ary[j]] = [tmp_ary[j], tmp_ary[ri]];
         }
 
-        // 長過ぎる部分を切りおとす
-        let del_len = tmp_ary.length - chromo_length;
-        tmp_ary.splice(del_len, del_len);
-
         // 個体の生成
         // 遺伝子は順序表現(残りの中で何番目にあるか)
         // ※0ベース
@@ -171,12 +171,12 @@ export class GrblFormGAOptimizer {
 
     // 実際に武器等の状態を作成する
     // [a, b] = [1, 2] -> a = 1; b = 2;
-    let [weapon_ga_ary, weapon_ga_param] = create_ga_state(this.weapon_ref.length, this.weapon_max_chromo_length, weapon_mutation);
-    let [summon_ga_ary, summon_ga_param] = create_ga_state(this.summon_ref.length, this.summon_max_chromo_length, summon_mutation);
-    let [friend_ga_ary, friend_ga_param] = create_ga_state(this.friend_ref.length, this.friend_max_chromo_length, friend_mutation);
+    let [weapon_ga_ary, weapon_ga_param] = create_ga_state(this.ref.weapon.length, this.weapon_max_chromo_length, weapon_mutation);
+    let [summon_ga_ary, summon_ga_param] = create_ga_state(this.ref.summon.length, this.summon_max_chromo_length, summon_mutation);
+    let [friend_ga_ary, friend_ga_param] = create_ga_state(this.ref.friend.length, this.friend_max_chromo_length, friend_mutation);
 
     // 最終的な初期状態を生成する
-    let ga_param = { population: [], weapon_param: weapon_ga_param, summon_param: summon_ga_param, friend_param: friend_ga_param };
+    let ga_param = { population: [], param: { weapon: weapon_ga_param, summon: summon_ga_param, friend: friend_ga_param, base: this.base } };
     for (let i = 0; i < population_length; i++) {
       let individual = { weapon: weapon_ga_ary[i], summon: summon_ga_ary[i], friend: friend_ga_ary[i], value: null };
       ga_param.population.push(individual);
@@ -194,28 +194,43 @@ export class GrblFormGAOptimizer {
    * objtorefによって作成されたリストはarytoobjで作成されたObjectを参照している。
    */
 
+  // 順序表現で記された染色体を元の表記を表わす配列に変換する関数
+  // 0ベース
+  conv_orderchromos_to_origchromos(chromos, length) {
+    let gene_array = [...Array(length).keys()];
+    let result = [];
+    for (let i of chromos) {
+      result.push(gene_array[i]);
+      gene_array.splice(i, 1);
+    }
+    return result;
+  }
+
+  // 元の表記(配列のindex)で表わされた遺伝子をキーの配列に変換する関数
+  conv_origchromos_to_objkeyarray(chromos, ref_array) {
+    return chromos.map((i) => { return ref_array[i] });
+  }
+
+  // オブジェクトのキー配列を遺伝子から生成する関数
+  conv_chromos_to_array(chromos, ref_array) {
+    let result = this.conv_origchromos_to_objkeyarray(
+      this.conv_orderchromos_to_origchromos(chromos, ref_array.length),
+      ref_array
+    );
+    return result;
+  }
+
+  // キーの配列からObjectの配列に変換する関数
+  objkeyarray_to_objarray(ary, ref_obj) {
+    let result = [];
+    ary.forEach(function (key) {
+      result.push(ref_obj[String(key)]);
+    });
+    return result;
+  }
+
   // individualとして渡された個体の価値を求める
   evaluate_value(individual) {
-    // 染色体をobjのキーを表わす配列に変換する関数
-    function conv_chromos_to_array(chromo, conv_array) {
-      let gene_array = [...Array(conv_array.length).keys()];
-      let result = [];
-      for (let i of chromo) {
-        result.push(gene_array[i]);
-        gene_array.splice(i, 1);
-      }
-      return result;
-    }
-
-    // キーの配列からObjectの配列に変換する関数
-    function objkeyarray_to_objarray(ary, ref_obj) {
-      let result = [];
-      ary.forEach(function (key) {
-        result.push(ref_obj[String(key)]);
-      });
-      return result;
-    }
-
     // キーの配列に対して同じキーを指しているものが「ない」かをチェックする
     function is_valid_key_array(ary) {
       let s = new Set(ary);
@@ -225,9 +240,9 @@ export class GrblFormGAOptimizer {
     // 個体の染色体をキーを表わす配列に変換する
     let [weapon_ary, summon_ary, friend_ary] =
     [
-      conv_chromos_to_array(individual.weapon, this.weapon_ref),
-      conv_chromos_to_array(individual.summon, this.summon_ref),
-      conv_chromos_to_array(individual.friend, this.friend_ref)
+      this.conv_chromos_to_array(individual.weapon, this.ref.weapon),
+      this.conv_chromos_to_array(individual.summon, this.ref.summon),
+      this.conv_chromos_to_array(individual.friend, this.ref.friend)
     ];
 
     // もし同じものを2度選択しているなら、その個体の価値は0
@@ -237,21 +252,24 @@ export class GrblFormGAOptimizer {
     if (valid_result.includes(false)) return 0;
 
     // キー値を示す配列からパラメータを生成する
-    let param_weapon = objkeyarray_to_objarray(weapon_ary, this.weapon_obj);
-    let param_summon = objkeyarray_to_objarray(summon_ary, this.summon_obj);
-    let param_friend = objkeyarray_to_objarray(friend_ary, this.friend_obj);
+    let param_weapon = this.objkeyarray_to_objarray(weapon_ary, this.weapon_obj);
+    let param_summon = this.objkeyarray_to_objarray(summon_ary, this.summon_obj);
+    let param_friend = this.objkeyarray_to_objarray(friend_ary, this.friend_obj);
 
     // 最終的な算出に使うパラメータを生成する
+    // TODO: ハードコーディングしてるのをどうにかする
+    // sliceは添字の*直前*まで取りだす
     let final_param = Object.assign({}, this.base, {
-      weapon: param_weapon,
-      summon: param_summon,
+      weapon: param_weapon.slice(0, 10),
+      summon: param_summon.slice(0, 5),
       friend: param_friend[0]
     });
 
     // 最終的な攻撃力を算出して返す
-    let result = calculate_atkval(final_param, this.job_data);
+    let result;
+    result = calculate_atkval(final_param, this.job_data);
     result = result.total_atk;
-    return result;
+    return [result, final_param];
   }
 
   // 各個体に価値(攻撃力)を設定する
@@ -267,8 +285,8 @@ export class GrblFormGAOptimizer {
       // 個体を取りだす
       let individual = this.state.ga_state.population[i];
       // 価値を付ける
-      let value = this.evaluate_value(individual);
-      this.state.ga_state.population[i].value = value;
+      let result = this.evaluate_value(individual);
+      this.state.ga_state.population[i].value = result[0];
       // 外に出すメッセージ用の文字列を設定する
       this.state.message = "ASSIGN VALUE: " + String(i+1) + "/" + String(l);
       // いったん中断する
@@ -297,7 +315,7 @@ export class GrblFormGAOptimizer {
 
   // 集団の選別をする関数
   *select_population() {
-    this.state.status = CALC_STATE.SELECTION_POPULATION;
+    this.state.status = CALC_STATE.SELECT_POPULATION;
     this.state.message = "START SELECTION";
     // 価値が無い個体を削除する
     this.state.ga_state.population = this.state.ga_state.population.filter((v) => {
@@ -310,20 +328,12 @@ export class GrblFormGAOptimizer {
       let max_value = this.state.ga_state.population[0].value;
       let population_length = this.state.ga_state.population.length;
       let min_value = this.state.ga_state.population[population_length - 1].value;
-      // 全体の半分以上になるまで選別する
+      // 全体の半分になるまで選別する
       // TODO: ハードコーディングしているのをどうにかする
       let target_length = Math.round(this.state.ga_state.population.length / 2);
-      let result_population = [];
-      while (result_population <= target_length) {
-        for (let v of this.state.ga_state.population) {
-          if (Math.random() < ((v.value - min_value) / (max_value - min_value))) {
-            result_population.push(v);
-          }
-        }
-        this.state.message = "SELECTION: " + String(result_population.length) + ":" + String(target_length);
-        yield this.state;
-      }
-      this.state.ga_state.population = result_population;
+      this.state.ga_state.population.splice((-1 * target_length), target_length);
+      this.state.message = "SELECTION:" + String(this.state.ga_state.population.length);
+      yield this.state;
     }
     this.state.message = "FINISH SELECTION";
     yield this.state;
@@ -331,26 +341,100 @@ export class GrblFormGAOptimizer {
 
   // 交叉を行なうジェネレータ関数
   *intersect() {
+    this.state.status = CALC_STATE.INTERSECT_GENE;
+    this.state.message = "START INTERSECTION";
     // 2つの遺伝子を受けとり交叉して新しい2つの遺伝子を返す関数
     // l_gene.length === r_gene.length
     let intersect_gene = (l_gene, r_gene) => {
-      let intersect_point = get_random_int(0, l_gene.length);
+      let intersection_point = get_random_int(0, l_gene.length);
       // 特殊な条件だけは別にする
-      if (intersect_point === l_gene.length) {
+      if (intersection_point === l_gene.length) {
         return [l_gene, r_gene];
-      } else if (intersect_point === 0) {
+      } else if (intersection_point === 0) {
         return [r_gene, l_gene];
       }
+      // 配列をカットする
+      let l_gene_cut = l_gene.splice(intersection_point, l_gene.length - intersection_point);
+      let r_gene_cut = r_gene.splice(intersection_point, r_gene.length - intersection_point);
+      // 交叉する
+      l_gene = l_gene.concat(r_gene_cut);
+      r_gene = r_gene.concat(l_gene_cut);
+
+      return [l_gene, r_gene];
     };
     // 必要な分だけ交叉して個体を集団に追加する
-    for (let i = this.state.population.length; i < this.state.max_population_length; i++) {
+    while (this.state.ga_state.population.length < this.state.ga_state.max_population_length) {
       // 交叉の対象となる2つの個体を選択する
-      let target = [0, 0];
-      while (target[0] === target[1]) {
-        target[0] = get_random_int(0, this.state.population.length - 1);
-        target[1] = get_random_int(0, this.state.population.length - 1);
+      // TODO: 1つしか個体が無かった場合の処理をどうにかする
+      let target_num = [0, 0];
+      while (target_num[0] === target_num[1]) {
+        target_num[0] = get_random_int(0, this.state.ga_state.population.length - 1);
+        target_num[1] = get_random_int(0, this.state.ga_state.population.length - 1);
+      }
+      // 交叉対象となる個体をコピーする
+      // そのまま代入すると元も変更されてしまう
+      let template_gene = {weapon: [], summon: [], friend: []};
+      let l_gene = Object.assign({}, template_gene);
+      let r_gene = Object.assign({}, template_gene);
+      l_gene.weapon = Array.from(this.state.ga_state.population[target_num[0]].weapon);
+      l_gene.summon = Array.from(this.state.ga_state.population[target_num[0]].summon);
+      l_gene.friend = Array.from(this.state.ga_state.population[target_num[0]].friend);
+      r_gene.weapon = Array.from(this.state.ga_state.population[target_num[1]].weapon);
+      r_gene.summon = Array.from(this.state.ga_state.population[target_num[1]].summon);
+      r_gene.friend = Array.from(this.state.ga_state.population[target_num[1]].friend);
+      // 交叉して結果をもらう
+      // intersect_geneは[遺伝子, 遺伝子]を返す
+      // TODO: データ構造が直前と違うのでどうにかしたい
+      let i_result = [Object.assign(template_gene), Object.assign(template_gene)];
+      [i_result[0].weapon, i_result[1].weapon] = intersect_gene(l_gene.weapon, r_gene.weapon);
+      [i_result[0].summon, i_result[1].summon] = intersect_gene(l_gene.summon, r_gene.summon);
+      [i_result[0].friend, i_result[1].friend] = intersect_gene(l_gene.friend, r_gene.friend);
+      this.state.ga_state.population = this.state.ga_state.population.concat(i_result);
+      this.state.message = "INTERSECTION: " + Number(this.state.ga_state.population.length);
+      yield this.state;
+    }
+    this.state.message = "FINISH INTERSECTION";
+    yield this.state;
+  }
+
+  // 突然変異を行なうジェネレータ関数
+  *mutation() {
+    // 変異前に状態変更
+    this.state.status = CALC_STATE.MUTATION;
+    this.state.message = "START MUTATION";
+    yield this.state;
+    // 変異の開始
+    const gene_type = ["weapon", "summon", "friend"];
+    for (let t of gene_type) {
+      this.state.message = "MUTATION: " + String(t);
+      yield this.state;
+      for (let individual of this.state.ga_state.population) {
+        // 変異する確率判定に成功したなら2つの遺伝子を入れかえる
+        if (Math.random() < this.state.ga_state.param[t].mutation_probability) {
+          let target_num = [0, 0];
+          if (individual[t].length != 1) {
+            while (target_num[0] === target_num[1]) {
+              target_num[0] = get_random_int(0, individual[t].length - 1);
+              target_num[1] = get_random_int(0, individual[t].length - 1);
+            }
+            let tmp_orig = this.conv_orderchromos_to_origchromos(individual[t], this.ref[t].length);
+            [tmp_orig[target_num[1]], tmp_orig[target_num[0]]] = [tmp_orig[target_num[0]], tmp_orig[target_num[1]]];
+            let tmp_indi = [];
+            let max_gene_number = this.state.ga_state.param[t].max_gene_number;
+            let gene_numbers = [...Array(max_gene_number).keys()];
+            for (let i of tmp_orig) {
+              let index = gene_numbers.indexOf(i);
+              tmp_indi.push(index);
+              gene_numbers.splice(index, 1);
+            }
+            individual[t] = tmp_indi;
+          }
+        }
       }
     }
+    // 変異後の状態変更
+    this.state.message = "FINISHED MUTATION";
+    yield this.state;
   }
 
   // 計算を進めるジェネレータ関数
@@ -365,11 +449,27 @@ export class GrblFormGAOptimizer {
     this.sort_population();
     yield this.state;
 
-    // 選別
-    yield* this.select_population();
+    for (let i = 0; i < 100; i++) {
+      // 選別
+      yield* this.select_population();
 
-    // ここらへんに交叉
+      // 交叉
+      yield* this.intersect();
+
+      // 突然変異
+      yield* this.mutation();
+
+      // 価値を設定してソート
+      yield* this.assign_value();
+      this.sort_population();
+      yield this.state;
+
+      // トップの価値をいったんメッセージにする
+      this.state.message = "CURRENT TOP: " + String(this.state.ga_state.population[0].value);
+      yield this.state;
+    }
 
     // ここらへんに終了条件判定
   }
+
 }
