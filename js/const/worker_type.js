db6a6033918a17b494ed499f8a986691b76c9542
philmist <philmist.j@gmail.com>
2016-10-12 21:38:04 +0900

    編成最適化計算機を実行させる前段階まで実装。

--- js/const/worker_type.js
+++ js/const/worker_type.js
@@ -6,7 +6,9 @@
  */
 
 export const WORKER_STATE = {
-  INITIAL: "INITIAL",  // 初期のまっさらな状態
+  STOP: "STOP",  // 停止している状態
+  INITED: "INITED",  // 初期化された状態
+  TERMINATE: "TERMINATE",  // 最適化ルーチンが中断された状態
   RUNNING: "RUNNING"  // GAによる最適化ルーチンが走っている状態
 };
 
@@ -16,5 +18,7 @@ export const WORKER_COMMAND = {
   SET_BASIC_INFO: "SET_BASIC_INFO",  // 基礎的な攻撃力等のパラメータを設定する
   SET_WEAPON: "SET_WEAPON",  // 武器の配列を設定する
   SET_SUMMON: "SET_SUMMON",  // 召喚の配列を設定する
-  SET_FRIEND: "SET_FRIEND"  // フレンド召喚の配列を設定する
+  SET_FRIEND: "SET_FRIEND",  // フレンド召喚の配列を設定する
+  INIT_OPTIMIZER: "INIT_OPTIMIZER",  // 最適化計算機を初期化する
+  TERMINATE_OPTIMIZER: "TERMINATE_OPTIMIZER"  // 計算機を中断させる
 };
