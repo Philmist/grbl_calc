94f5671ad82941d72667e4b7da99ba463d81f23f
philmist <philmist.j@gmail.com>
2016-10-21 14:43:10 +0900

    ジョブデータ定数にコメントを付けた。

--- js/const/job_data.js
+++ js/const/job_data.js
@@ -1,7 +1,14 @@
+// vim: set sw=2 sts=2 ts=2 expandtab
+
+/*
+ * ジョブの基礎的な情報を記した
+ * 定数
+ */
+
 let job_data = {
-  "fighter": {
-    "specialty" : ["sword", "axe"],
-    "atk_bonus" : 1500,
+  "fighter": {  // 職の英語名(計算機内部で参照される)
+    "specialty" : ["sword", "axe"],  // 武器の英語名(計算機内部と表示で参照される)
+    "atk_bonus" : 1500,  // 攻撃力ボーナス
   },
   "knight": {
     "specialty" : ["sword", "spear"],
