011b83baeaae721a8a0097c453a145100a916257
Philmist <philmist.j@gmail.com>
2016-11-03 16:58:31 +0900

    N,R,SR,SSRのおおよその武器を取得できるようにした。

--- scripts/test_scraping.py
+++ scripts/test_scraping.py
@@ -5,57 +5,96 @@ import requests
 import json
 
 # SSR武器ページを読みこむ
-request = requests.get(r'http://mirror.gbf-wiki.com/index.php?%C9%F0%B4%EFSSR')
-soup = BeautifulSoup(request.text, 'html.parser')
+request_ssr = requests.get(r'http://mirror.gbf-wiki.com/index.php?%C9%F0%B4%EFSSR') # SSRはスキル2つの可能性あり
+request_sr = requests.get(r'http://mirror.gbf-wiki.com/index.php?%C9%F0%B4%EFSR')  # SRはスキル2つの可能性あり\
+request_r = requests.get(r'http://mirror.gbf-wiki.com/index.php?%C9%F0%B4%EFR')  # Rはスキル1つ
+request_n = requests.get(r'http://mirror.gbf-wiki.com/index.php?%C9%F0%B4%EFN')  # Nはスキル無し
+
+soup = {
+    'SSR': BeautifulSoup(request_ssr.text, 'html.parser'),
+    'SR': BeautifulSoup(request_sr.text, 'html.parser'),
+    'R': BeautifulSoup(request_r.text, 'html.parser'),
+    'N': BeautifulSoup(request_n.text, 'html.parser')
+    }
 
 # 通常武器一覧を抜きだす
-rows = soup.find('h2', id='content_1_1').find_next('table').find_all('tr')
-rows = rows[1:]  # タイトルはいらない
+items = {k: v.find('h2', id='content_1_1').find_next('table').find_all('tr') for k, v in soup.items()}
+items = {k: v[1:] for k, v in items.items()}  # タイトルはいらない
 
 # 武器一覧を格納するリストを作る
 weapons = list()
 
 # 武器一覧を一行ずつなめていく
-for row in rows:
-    # 武器名等を入れる辞書を作る
-    weapon = dict()
-    # td要素を全て抜きだす
-    tds = row.find_all('td')
-    # 武器名の抽出
-    name = tds[1]
-    while (name.br):
-        name.br.decompose()
-    while (name.a):
-        name.a.decompose()
-    print(name.contents)
-    weapon['name'] = ''.join(name.contents)
-    # 属性の抽出
-    attr = tds[2]
-    attr.span.unwrap()
-    weapon['attribute'] = ''.join(attr.contents)
-    # 武器種別の抽出
-    w_type = tds[3]
-    weapon['type'] = ''.join(w_type.contents)
-    # MinHPの抽出
-    min_hp = tds[8]
-    weapon['min_hp'] = int(''.join(min_hp.contents))
-    # MinATKの抽出
-    min_atk = tds[9]
-    weapon['min_atk'] = int(''.join(min_atk.contents))
-    # maxHPの抽出
-    max_hp = tds[10]
-    try:
-        weapon['max_hp'] = int(''.join(max_hp.contents))
-    except ValueError:
-        weapon['max_hp'] = weapon['min_hp']
-    # maxATKの抽出
-    max_atk = tds[11]
-    try:
-        weapon['max_atk'] = int(''.join(max_atk.contents))
-    except ValueError:
-        weapon['max_atk'] = weapon['min_atk']
-    # 武器一覧に格納
-    weapons.append(weapon)
+for rarelity, rows in items.items():
+    for row in rows:
+        # 武器名等を入れる辞書を作る
+        weapon = dict()
+        # td要素を全て抜きだす
+        tds = row.find_all('td')
+        # 武器名の抽出
+        name = tds[1]
+        while (name.br):
+            name.br.decompose()
+        while (name.a):
+            name.a.decompose()
+        print(name.contents)
+        weapon['name'] = ''.join(name.contents)
+        # 属性の抽出
+        attr = tds[2]
+        attr.span.unwrap()
+        weapon['attribute'] = ''.join(attr.contents)
+        # 武器種別の抽出
+        w_type = tds[3]
+        weapon['type'] = ''.join(w_type.contents)
+        # スキル文字列の抽出
+        if (rarelity == 'R'):
+            s_type_tmp = [tds[6]]
+        elif (rarelity == 'N'):
+            s_type_tmp = []
+        else:
+            s_type_tmp = [tds[6], tds[7]]
+        s_type = []
+        for s in s_type_tmp:
+            if(s.a):
+                s.a.unwrap()
+            while(s.a):
+                s.a.decompose()
+            try:
+                s_type.append(''.join(s.contents))
+            except TypeError:
+                s_type.append('')
+        weapon['skill'] = s_type
+        # MinHPの抽出
+        if (rarelity == 'N' or rarelity == 'R'):
+            min_hp = tds[7]
+        else:
+            min_hp = tds[8]
+        weapon['min_hp'] = int(''.join(min_hp.contents))
+        # MinATKの抽出
+        min_atk = tds[9]
+        weapon['min_atk'] = int(''.join(min_atk.contents))
+        # maxHPの抽出
+        if (rarelity == 'N' or rarelity == 'R'):
+            mas_hp = tds[9]
+        else:
+            max_hp = tds[10]
+        try:
+            weapon['max_hp'] = int(''.join(max_hp.contents))
+        except ValueError:
+            weapon['max_hp'] = weapon['min_hp']
+        # maxATKの抽出
+        if (rarelity == 'N' or rarelity == 'R'):
+            max_atk = tds[10]
+        else:
+            max_atk = tds[11]
+        try:
+            weapon['max_atk'] = int(''.join(max_atk.contents))
+        except ValueError:
+            weapon['max_atk'] = weapon['min_atk']
+        # レアリティを格納
+        weapon['rarelity'] = rarelity
+        # 武器一覧に格納
+        weapons.append(weapon)
 
 json_data = {'WEAPONS': weapons}
 
