def33be765aa092cbd0995c2cc5b007dfc23f06c
philmist <philmist.j@gmail.com>
2018-12-12 01:23:47 +0900

    最新のWebpackとBabelで使えるようにした。

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 // vim: sts=2 sw=2 ts=2 expandtab
-// Webpack2用のコンフィグファイルです。
-// 必ず Webpack@~2.1.0-beta 以上で使ってください。
+// Webpack4用のコンフィグファイルです。
 const path = require("path");
 const webpack = require("webpack");
 
@@ -8,65 +7,54 @@ const webpack = require("webpack");
 // 参考: https://github.com/ModusCreateOrg/budgeting-sample-app-webpack2/blob/master/webpack.config.js
 const nodeEnv = process.env.NODE_ENV || "development";
 const isProd = nodeEnv === "production";
+const strProd = isProd ? "production" : "development";
 
 // ローダー(ファイル読み込み用のスクリプト)の設定
-let loaders = [
+let rules = [
   {
     // CSS Modules(https://github.com/css-modules/css-modules)
     // を使うための設定
     test: /\.css$/,
-    loader: [
-      { loader: "style-loader", query: { sourceMap: true } },
-      { loader: "css-loader", query: { modules: true, importLoaders: 1, localIdentName: "[name]__[local]___[hash:base64:5]" } }
-    ]
+    use: 
+      [
+        { loader: "style-loader", options: { sourceMap: true } },
+        { loader: "css-loader", options: { modules: true, importLoaders: 1, localIdentName: "[name]__[local]___[hash:base64:5]" } }
+      ]
   },
   {
-    // JSXとJSを使うための設定 (Babel6で変換します)
+    // JSXとJSを使うための設定 (Babelで変換します)
     // 正規表現なので.jsと.jsxの両方がひっかかります
     test: /\.jsx?$/,
-    loader: "babel-loader",
-    // node_modulesとかの中を変換されてもちょっと困る
-    exclude: /(node_modules|bower_components)/,
-    query: {
-      plugins: ['transform-runtime'],
-      // プリセットは順序が大事
-      presets: ['react', ['es2015', { module: false }], 'stage-0'],
-    }
+    use: [{ 
+      loader: "babel-loader",
+      // node_modulesとかの中を変換されてもちょっと困る
+      options: {
+        plugins: [
+          ['@babel/transform-runtime', { useESModules: true }],
+          '@babel/plugin-proposal-function-bind'
+        ],
+        // プリセットは順序が大事
+        presets: [
+          '@babel/preset-react',
+          ['@babel/preset-env', { modules: false }]
+        ],
+      }
+    }]
   },
   {
     test: /\.(woff|ttf|eot|svg)$/,
-    loader: [
+    use: [
       { loader: "url-loader", query: { limit: 30000, name: "./dist/[name]-[hash].[ext]" } }
     ]
   }
 ];
 
 
-// プラグイン(webpackで使われてファイルを変換したりする)の設定
-let plugins = [
-  new webpack.DefinePlugin({
-    "process.env": { NODE_ENV: JSON.stringify(nodeEnv) }
-  }),
-  new webpack.LoaderOptionsPlugin({
-    minimize: isProd,
-    debug: isProd
-  })
-];
-if (isProd) {
-  plugins.push(
-    new webpack.optimize.UglifyJsPlugin({
-      compress: { warnings: false },
-      output: { comments: true },
-      sourceMap: false
-    })
-  );
-}
-
 
 // webpack(1|2)は同時にいくつかのファイルをバンドルできます
 module.exports = [
   {  // 計算機のメイン(エントリポイント)
-    devtool: isProd ? "#hidden-source-map" : "#source-map",
+    devtool: isProd ? "hidden-source-map" : "source-map",
     entry: {
       js: "./js/entry.jsx",
     },
@@ -74,7 +62,8 @@ module.exports = [
       filename: "./dist/bundle.js",
     },
     module: {
-      loaders: loaders,
+      rules: rules,
+      noParse: /(node_modules|bower_components)/
     },
     resolve: {
       extensions: [".js", ".jsx", ".css"],
@@ -82,10 +71,10 @@ module.exports = [
         "node_modules", "js", "css"
       ]
     },
-    plugins: plugins
+    mode: strProd
   },
   {  // 編成計算機のWebWorkerファイル(必要最低限)
-    devtool: isProd ? "#hidden-source-map" : "#source-map",
+    devtool: isProd ? "hidden-source-map" : "source-map",
     entry: {
       js: "./js/worker_optimizer.js",
     },
@@ -93,7 +82,8 @@ module.exports = [
       filename: "./dist/optimizer.js",
     },
     module: {
-      loaders: loaders,
+      rules: rules,
+      noParse: /(node_modules|bower_components)/
     },
     resolve: {
       extensions: [".js"],
@@ -101,6 +91,6 @@ module.exports = [
         "node_modules", "js"
       ]
     },
-    plugins: plugins
+    mode: strProd
   }
 ];
